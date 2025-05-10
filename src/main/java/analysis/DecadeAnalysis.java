1716b089ed21b859b04685db95f8bc575187905f
Tobias Kothbauer <101700683+TobiasKothbauer@users.noreply.github.com>
2025-05-10 21:52:42 +0200

    [RQ3]: Implement decade analysis. ETL, storage, analysis, output

---
+++ src/main/java/analysis/DecadeAnalysis.java
@@ -0,0 +1,71 @@
+package analysis;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+
+import static org.apache.spark.sql.functions.*;
+
+/**
+ * Computes decade-level averages for positiveness, tempo, and energy.
+ */
+public class DecadeAnalysis {
+
+    private static final String PARQUET_OUTPUT_PATH = "data/processed/decade_genre_analysis.parquet";
+
+    public static void main(String[] args) {
+        System.setProperty("hadoop.home.dir", "C:/Users/43650/hadoop/");
+        System.load("C:/Users/43650/hadoop/bin/hadoop.dll");
+
+        SparkSession spark = SparkSession.builder()
+                .appName("Song Decade Analysis")
+                .master("local[*]")
+                .config("spark.driver.bindAddress", "127.0.0.1")
+                .getOrCreate();
+
+        Dataset<Row> df = spark.read()
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .option("multiLine", true)
+                .option("escape", "\"")
+                .csv("data/raw/spotify_dataset.csv");
+
+        // Clean column for easier access
+        df = df.withColumnRenamed("Release Date", "ReleaseDate");
+
+        // Add Decade column
+        Dataset<Row> transformedDF = df.withColumn("decade",
+                expr("floor(year(ReleaseDate) / 10) * 10")
+        );
+
+        // Write to Parquet, partinioned by decade
+        transformedDF.write()
+                .partitionBy("decade")
+                .mode(SaveMode.Overwrite)
+                .parquet(PARQUET_OUTPUT_PATH);
+
+        // Read back the parquet with automatic partition discovery
+        Dataset<Row> parquetDF = spark.read()
+                .parquet(PARQUET_OUTPUT_PATH);
+
+        // DataFrame API
+        Dataset<Row> analysisResult = parquetDF.groupBy("decade")
+                .agg(
+                        avg("Positiveness").alias("avg_positiveness"),
+                        avg("Tempo").alias("avg_tempo"),
+                        avg("Energy").alias("avg_energy"),
+                        count("*").alias("n_songs")
+                )
+                .orderBy("decade");
+
+        analysisResult.show(20, false);
+
+        // Save the result for later -> tableau?
+        analysisResult.write()
+                .mode(SaveMode.Overwrite)
+                .parquet("data/processed/decade_analysis_result.parquet");
+
+        spark.stop();
+    }
+}
